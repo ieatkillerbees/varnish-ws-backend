9c9a5734dc622d25bf27b30e85bb0d3d78f583a3
Samantha Quiñones <samantha.quinones@teamaol.com>
2014-11-10 16:52:54 -0500

    Testing json again

--- backend.php
+++ backend.php
@@ -23,17 +23,9 @@ $app->after(function (Request $request, Response $response) {
 	$response->setSharedMaxAge(300);
 	$response->setVary('X-OS');
 	$response->setPublic();
-	$stream_response = new StreamedResponse();
-	$stream_response->setCallback(function () use ($response) {
-		echo $response->content();
-		flush();
-	});
-//	$response->send();
-//	$response->headers->set('Content-Length', strlen($response->getContent()));
 });
 
 $app->get('/', function(Application $app) {
-	return $app['twig']->render('cached.twig', ['timestamp' => date('r')]);
 	return new \Symfony\Component\HttpFoundation\JsonResponse([
 		'timestamp' => date('r'),
 		'max-age' => 300
