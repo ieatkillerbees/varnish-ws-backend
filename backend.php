dc4ab897dfef5a6d20a151f103ddfa4ac242adf7
Samantha Quiñones <samantha.quinones@teamaol.com>
2014-11-10 16:26:23 -0500

    Testing stream

--- backend.php
+++ backend.php
@@ -4,6 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 $app = new Application();
 $app['debug'] = true;
@@ -22,10 +23,17 @@ $app->after(function (Request $request, Response $response) {
 	$response->setSharedMaxAge(300);
 	$response->setVary('X-OS');
 	$response->setPublic();
+	$stream_response = new StreamedResponse();
+	$stream_response->setCallback(function () use ($response) {
+		echo $response->content();
+		flush();
+	});
+	$response->send();
 	$response->headers->set('Content-Length', strlen($response->getContent()));
 });
 
 $app->get('/', function(Application $app) {
+	return $app['twig']->render('cached.twig', ['timestamp' => date('r')]);
 	return new \Symfony\Component\HttpFoundation\JsonResponse([
 		'timestamp' => date('r'),
 		'max-age' => 300
