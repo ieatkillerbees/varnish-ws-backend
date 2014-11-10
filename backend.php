71e8fb7e5c947e1035dc7bf8a8736d7a36100817
Samantha Quiñones <samantha@tembies.com>
2014-11-10 17:02:55 +0000

    Updates

--- backend.php
+++ backend.php
@@ -1,23 +1,22 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app = new Application();
-var_dump($app); die;
-require __DIR__ . '/config/conf_' . $_SERVER['SERVER_PORT'] . '.php';
+$app['debug'] = true;
+//require __DIR__ . '/config/conf_' . $_SERVER['SERVER_PORT'] . '.php';
+$app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__ . '/views']);
 
-$app->after(
-	/**
-	 * @param Request $request
-	 * @param Response $response
-	 */
-	function (Request $request, Response $response) {
-		$response->headers->set('X-Backend-Port', $_SERVER['SERVER_PORT']);
-	}
-);
+$app->after(function (Request $request, Response $response) {
+	$response->setMaxAge(30);
+	$response->setPublic();
+	$response->headers->set('Content-Length', strlen($response->getContent()));
+	$response->headers->set('Vary', 'X-OS');
+});
 
-$app->get('/', function () {
-	return 'The current server time is: ' . date('r');
+$app->get('/', function(Application $app) {
+	return $app['twig']->render('cached.twig', ['timestamp' => date('r')]);
 });
